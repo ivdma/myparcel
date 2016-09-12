bc115d67db8bb7dded144e08ae5eb3d977221563
Ivan Malykh <ivan@lesslines.com>
2016-09-12 19:27:38 +0200

    added libraries and files

--- lib/myparcel.rb
+++ lib/myparcel.rb
@@ -1,5 +1,11 @@
-require "myparcel/version"
+require 'active_interaction'
+require 'httparty'
+require 'base64'
 
+require 'myparcel/version'
+require 'myparcel/endpoint'
+require 'myparcel/list_shipments'
+
+# Ruby API Wrapper for https://api.myparcel.nl
 module Myparcel
-  # Your code goes here...
 end
