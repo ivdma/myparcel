676b09d8229efe182a461edefcabf658b5f19a5f
Ivan Malykh <ivan@lesslines.com>
2016-09-13 20:19:10 +0200

    add address

---
+++ lib/myparcel/models.rb
@@ -0,0 +1,5 @@
+require 'myparcel/models/address'
+require 'myparcel/models/price'
+
+# require 'myparcel/models/shipment'
+# require 'myparcel/models/address'
---
+++ lib/myparcel/models/address.rb
@@ -0,0 +1,19 @@
+module Myparcel
+  module Models
+    class Address
+      include Virtus.model
+
+      attribute :cc,                     String
+      attribute :postal_code,            String
+      attribute :city,                   String
+      attribute :street,                 String
+      attribute :street_additional_info, String
+      attribute :number,                 Integer
+      attribute :number_suffix,          String
+      attribute :person,                 String
+      attribute :company,                String
+      attribute :email,                  String
+      attribute :phone,                  String
+    end
+  end
+end
