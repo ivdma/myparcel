bd33219425dc25c0206878924ab9c579b6eeb741
Ivan Malykh <ivan@lesslines.com>
2016-09-22 19:19:23 +0200

    add models file

--- lib/myparcel/models.rb
+++ lib/myparcel/models.rb
@@ -1,7 +1,9 @@
 require 'myparcel/models/address'
 require 'myparcel/models/price'
 
-require 'myparcel/models/custom_object'
-
-# require 'myparcel/models/shipment'
-# require 'myparcel/models/address'
+require 'myparcel/models/custom_item'
+require 'myparcel/models/pickup_location'
+require 'myparcel/models/price'
+require 'myparcel/models/shipment_general_settings'
+require 'myparcel/models/shipment_options'
+require 'myparcel/models/shipment'
