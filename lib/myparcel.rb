2e60f5ccf4ddd46aa889969e031dea8988da99f0
Ivan Malykh <ivan@lesslines.com>
2016-10-04 23:03:59 +0200

    require correct files

--- lib/myparcel.rb
+++ lib/myparcel.rb
@@ -1,10 +1,11 @@
 require 'active_interaction'
 require 'httparty'
+require 'virtus'
 require 'base64'
 
 require 'myparcel/version'
 require 'myparcel/endpoint'
-require 'myparcel/list_shipments'
+require 'myparcel/models'
 
 # Ruby API Wrapper for https://api.myparcel.nl
 module Myparcel
