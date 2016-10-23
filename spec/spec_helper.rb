274469ce0e41e137ebc50e9ed86cf9c53a0c83d0
Ivan Malykh <ivan@lesslines.com>
2016-10-23 14:06:50 +0200

    get shipments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,14 @@ require 'myparcel'
 require 'webmock'
 require 'vcr'
 require 'timecop'
+require 'dotenv'
+
+Dotenv.load
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock # or :fakeweb
+  config.allow_http_connections_when_no_cassette = false
 end
 
 RSpec.configure do |config|
