21754cca5348bb30e9ba82fabc21a893ab9abae9
Ivan Malykh <ivan@lesslines.com>
2016-10-23 14:17:10 +0200

    remove unneeded stuff

--- lib/myparcel/endpoint.rb
+++
@@ -1,6 +0,0 @@
-module Myparcel
-  # Base structure for enpoints
-  class Endpoint < ActiveInteraction::Base
-    string :api_key, default: -> { ENV['MYPARCEL_API_KEY'] }
-  end
-end
--- spec/myparcel/endpoint_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Myparcel::Endpoint do
-  pending 'write specs for this class'
-end
