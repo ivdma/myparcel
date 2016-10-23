f58005ed6ecdcfe76e92d0811f1611f8f6771a86
Ivan Malykh <ivan@lesslines.com>
2016-10-23 14:15:58 +0200

    authentication

--- lib/myparcel/authentication.rb
+++ lib/myparcel/authentication.rb
@@ -1,15 +1,18 @@
 module Myparcel
+  # Creates authentication headers
   class Authentication
+    attr_accessor :api_key
+
+    def initialize(api_key = ENV['MYPARCEL_API_KEY'])
+      @api_key = api_key
+    end
+
     def headers
       { 'Authorization' => "basic #{auth_token}" }
     end
 
     def auth_token
       Base64.urlsafe_encode64(api_key)
-    end
-
-    def api_key
-      ENV['MYPARCEL_API_KEY'] || ''
     end
 
     def valid?
--- spec/myparcel/authentication_spec.rb
+++ spec/myparcel/authentication_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe Myparcel::Authentication do
+  subject(:authentication) { described_class.new }
+
   before do
     allow(ENV).to receive(:[]).with('MYPARCEL_API_KEY').and_return('foo')
   end
 
-  subject(:authentication) { described_class.new }
+  describe 'initialization' do
+    it 'uses provided api key' do
+      authentication = described_class.new('bar')
+      expect(authentication.api_key).to eq 'bar'
+    end
+
+    it 'alls back to env var' do
+      expect(authentication.api_key).to eq 'foo'
+    end
+  end
 
   describe '#headers' do
     it 'uses auth_token to create auth header' do
@@ -16,12 +27,6 @@ describe Myparcel::Authentication do
   describe '#auth_token' do
     it 'makes encoded token' do
       expect(authentication.auth_token).to eq Base64.urlsafe_encode64('foo')
-    end
-  end
-
-  describe '#api_key' do
-    it 'reads the key from environment' do
-      expect(authentication.api_key).to eq 'foo'
     end
   end
 
